8e0854e711967f77c99ed2ab99798517e5b56f4a
Kaushik Biswas <76803260+kaushik-vishwas@users.noreply.github.com>
2023-08-05 01:25:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
         const submitBtn = document.getElementById("submitBtn");
         submitBtn.addEventListener("click", function()
         {
+        const mainTxt = document.getElementById('mainTxt')
         if(mainTxt!=null)
              mainTxt.remove();
-        const mainTxt = document.getElementById('mainTxt')
         const searchPic = document.getElementById("searchPic").value;
         const numOfPic = document.getElementById("numOfPic").value;
           async function fetchAndDisplayImages() {
